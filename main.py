7029efaa585ea1d9c7b94f861755d5ca049d4acd
Pyramorphix <zarembo.vv@phystech.edu>
2024-04-08 09:49:15 +0300

    Migrate to mp.hands instead of holistics

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 2)
 
 OUTPUT = False
-# OUTPUT = True
+OUTPUT = True
 
 try:
-    with mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    with mp.solutions.hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=1) as hands:
 
         current = None
         init_counter = 0
@@ -42,50 +42,50 @@ try:
 
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             t = time.time()
-            results = holistic.process(frame_rgb)
+            results = hands.process(frame_rgb)
             dt = time.time() - t
 
             # print("dt=%f" % dt)
-            if results.right_hand_landmarks is not None:
-                index = [
-                    results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_MCP],
-                    results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_PIP],
-                    results.right_hand_landmarks.landmark[mp.solutions.holistic.HandLandmark.INDEX_FINGER_TIP],
-                ]
-                index = [(l.x, l.y) for l in index]
-                scalar = (index[2][0] - index[1][0]) * (index[1][0] - index[0][0]) + (index[2][1] - index[1][1]) * (index[1][1] - index[0][1])
-                print(scalar)
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    index = [
+                        hand_landmarks.landmark[mp.solutions.hands.HandLandmark.INDEX_FINGER_MCP],
+                        hand_landmarks.landmark[mp.solutions.hands.HandLandmark.INDEX_FINGER_PIP],
+                        hand_landmarks.landmark[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP],
+                    ]
+                    index = [(l.x, l.y) for l in index]
+                    scalar = (index[2][0] - index[1][0]) * (index[1][0] - index[0][0]) + (index[2][1] - index[1][1]) * (index[1][1] - index[0][1])
+                    print(scalar)
 
-                if scalar > 0:
-                    init_counter += 1
-                    if init_counter > 2:
-                        if current is None:
-                            current = new_gesture()
-                            print("New")
-                            init = index[0]
-                        else:
-                            print("Update")
-                            current.Update(["delta_x", "delta_y"], [- (index[0][0] - init[0]), index[0][1] - init[1]])
-                else:
-                    init_counter = 0
-                    if current is not None:
-                        print("Terminate")
-                        current.Terminate()
-                        current = None
+                    if scalar > 0:
+                        init_counter += 1
+                        if init_counter > 2:
+                            if current is None:
+                                current = new_gesture()
+                                print("New")
+                                init = index[0]
+                            else:
+                                print("Update")
+                                current.Update(["delta_x", "delta_y"], [- (index[0][0] - init[0]), index[0][1] - init[1]])
+                    else:
+                        init_counter = 0
+                        if current is not None:
+                            print("Terminate")
+                            current.Terminate()
+                            current = None
+
+                if OUTPUT:
+                    mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks, mp.solutions.holistic.HAND_CONNECTIONS)
             else:
                 init_counter = 0
                 if current is not None:
                     print("Terminate")
                     current.Terminate()
                     current = None
-
+            
             if OUTPUT:
-                mp.solutions.drawing_utils.draw_landmarks(frame, results.face_landmarks, mp.solutions.holistic.FACEMESH_CONTOURS)
-                mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp.solutions.holistic.POSE_CONNECTIONS)
-                mp.solutions.drawing_utils.draw_landmarks(frame, results.left_hand_landmarks, mp.solutions.holistic.HAND_CONNECTIONS)
-                mp.solutions.drawing_utils.draw_landmarks(frame, results.right_hand_landmarks, mp.solutions.holistic.HAND_CONNECTIONS)
-
                 cv2.imshow('Preview', frame)
+
 finally:
     cap.release()
     cv2.destroyAllWindows()
